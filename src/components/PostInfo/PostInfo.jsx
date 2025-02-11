61f14ea27493478d69ced3b0b895bfd7ca79fe63
marushchak <marushchak099@gmail.com>
2025-02-11 13:07:02 +0200

    for chack

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,11 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo';
+
+export const PostInfo = ({ post }) => (
+  <>
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+      <UserInfo user={post.user} />
+    </div>
+    <p className="PostInfo__body">{post.body}</p>
+  </>
+);
